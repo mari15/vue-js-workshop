5e23a62026b41793ced63bbf66d4fc30490ccb34
DESKTOP-IHRE5VV\Vlad <gati_mariana@yahoo.com>
2021-12-18 17:15:12 +0200

    move button to components

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+   <Button text="Click me"/>
+</template>
+
+<script>
+// @ is an alias to /src
+import Button from "@/components/Button.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Button,
+  },
+};
+</script>
